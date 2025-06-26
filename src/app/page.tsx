56d957aad67431234e197be6d051066485537d1b
DiyanshuTiwaari <sontitiwa569@gmail.com>
2025-06-26 09:51:59 +0530

    mess

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ import CTASection from './component/cta/page';
 import Footer from './component/footer/page';
 import FeaturesSection from './component/feature section/page';
 import WhyFeatures from './component/whyhere/page';
-import Timeline from './component/ball/page';
+
 import PricingSection from './component/pricing/page';
 import ProjectShowcase from './component/projects/page';
 
